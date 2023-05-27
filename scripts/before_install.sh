dcf29fb8d072ad1484de5709499f293c28065979
bipinthakur52 <bipinthakur52@gmail.com>
2023-05-27 23:59:45 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,6 @@ parameter_name="/myapp/mykey"
 
 # Retrieve the parameter value
 #parameter_value=$(aws ssm get-parameter --name "$parameter_name" --query "Parameter.Value" --output text)
-
+echo "Bipin singh"
 # Use the parameter value in your script
 echo "Parameter Value: $dbname"
