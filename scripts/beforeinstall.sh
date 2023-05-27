36d3dd692de61afcf88e55421f79c1e459dd72d2
bipinthakur52 <bipinthakur52@gmail.com>
2023-05-27 22:14:25 +0530

    Delete beforeinstall.sh

--- scripts/beforeinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Retrieve the branch name from the CodeBuild environment variable
-BRANCH_NAME=$(echo $CODEBUILD_SOURCE_VERSION | awk -F "/" '{print $3}')
-
-# Use the branch name in your script
-echo "The branch name is: $BRANCH_NAME"
-
-# Additional commands using the branch name
