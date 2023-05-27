62b9816c72c9450ddd6a39a3791bf1006700f6be
bipinthakur52 <bipinthakur52@gmail.com>
2023-05-27 22:23:40 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 # Retrieve the branch name from the CodeBuild environment variable
 BRANCH_NAME=$(echo $CODEBUILD_SOURCE_VERSION | awk -F "/" '{print $3}')
-
+echo "=================== Bipin ======================="
 # Use the branch name in your script
 echo "The branch name is: $BRANCH_NAME"
 
