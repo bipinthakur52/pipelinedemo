75e0fc19bf90fc2d77d16a40e4271a7c0c74cd2f
bipinthakur52 <bipinthakur52@gmail.com>
2023-05-28 23:43:34 +0530

    Update post_install.sh

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Set the BASEDIR variable
+BASEDIR="/home/ubuntu/djangocode/django-helloworld-master/"
+cd "$BASEDIR"
+# Install Python 3.9
+sudo apt-get install python3.9
+
+# Create and activate virtual environment
+python3.9 -m venv env
+source "${BASEDIR}env/bin/activate"
+
+# Change to the source code directory
+cd "$BASEDIR"
+
+# Install required packages
+pip install -r requirements.txt
