e2c49fa1c427f7c4c6d929062b0cf4a6b4dab7c2
bipinthakur52 <bipinthakur52@gmail.com>
2023-05-28 23:45:31 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Set the BASEDIR variable
+BASEDIR="/home/ubuntu/djangocode/django-helloworld-master/"
+# Change to the BASEDIR
+cd "$BASEDIR"
+# Clear static files
+python manage.py collectstatic --clear --no-input
+
+# Make migrations
+python manage.py makemigrations
+
+# Apply migrations
+python manage.py migrate
+
+# Restart Gunicorn (assuming it's managed by systemd)
+sudo systemctl restart gunicorn
