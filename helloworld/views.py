90c14bc9783f5e074929dc58d2456004eb87ff6f
bipinthakur52 <bipinthakur52@gmail.com>
2023-05-28 14:06:18 +0530

    Update views.py

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 import os
 def index(request):
-    return HttpResponse("Hello, pipelinebipinthakur with testing"+os.environ['DBNAME'])
+    return HttpResponse("Hello, pipelinebipinthakur with testing"+os.environ['LAMBDA_SQL_ENGINE'])
